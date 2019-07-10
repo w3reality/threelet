0b0e94fa690af3894ff607cce4e821cf2c915c20
jayd <j@w3reality.com>
2019-07-10 12:40:11 +0700

    Add wasm-life example (WIP)

---
+++ examples/wasm-life/wasm_game_of_life.js
@@ -0,0 +1,224 @@
+// import * as wasm from './wasm_game_of_life_bg';
+
+const heap = new Array(32);
+
+heap.fill(undefined);
+
+heap.push(undefined, null, true, false);
+
+let heap_next = heap.length;
+
+function addHeapObject(obj) {
+    if (heap_next === heap.length) heap.push(heap.length + 1);
+    const idx = heap_next;
+    heap_next = heap[idx];
+
+    heap[idx] = obj;
+    return idx;
+}
+
+function getObject(idx) { return heap[idx]; }
+
+let WASM_VECTOR_LEN = 0;
+
+let cachedTextEncoder = new TextEncoder('utf-8');
+
+let cachegetUint8Memory = null;
+function getUint8Memory() {
+    if (cachegetUint8Memory === null || cachegetUint8Memory.buffer !== wasm.memory.buffer) {
+        cachegetUint8Memory = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachegetUint8Memory;
+}
+
+let passStringToWasm;
+if (typeof cachedTextEncoder.encodeInto === 'function') {
+    passStringToWasm = function(arg) {
+
+
+        let size = arg.length;
+        let ptr = wasm.__wbindgen_malloc(size);
+        let offset = 0;
+        {
+            const mem = getUint8Memory();
+            for (; offset < arg.length; offset++) {
+                const code = arg.charCodeAt(offset);
+                if (code > 0x7F) break;
+                mem[ptr + offset] = code;
+            }
+        }
+
+        if (offset !== arg.length) {
+            arg = arg.slice(offset);
+            ptr = wasm.__wbindgen_realloc(ptr, size, size = offset + arg.length * 3);
+            const view = getUint8Memory().subarray(ptr + offset, ptr + size);
+            const ret = cachedTextEncoder.encodeInto(arg, view);
+
+            offset += ret.written;
+        }
+        WASM_VECTOR_LEN = offset;
+        return ptr;
+    };
+} else {
+    passStringToWasm = function(arg) {
+
+
+        let size = arg.length;
+        let ptr = wasm.__wbindgen_malloc(size);
+        let offset = 0;
+        {
+            const mem = getUint8Memory();
+            for (; offset < arg.length; offset++) {
+                const code = arg.charCodeAt(offset);
+                if (code > 0x7F) break;
+                mem[ptr + offset] = code;
+            }
+        }
+
+        if (offset !== arg.length) {
+            const buf = cachedTextEncoder.encode(arg.slice(offset));
+            ptr = wasm.__wbindgen_realloc(ptr, size, size = offset + buf.length);
+            getUint8Memory().set(buf, ptr + offset);
+            offset += buf.length;
+        }
+        WASM_VECTOR_LEN = offset;
+        return ptr;
+    };
+}
+
+let cachegetUint32Memory = null;
+function getUint32Memory() {
+    if (cachegetUint32Memory === null || cachegetUint32Memory.buffer !== wasm.memory.buffer) {
+        cachegetUint32Memory = new Uint32Array(wasm.memory.buffer);
+    }
+    return cachegetUint32Memory;
+}
+
+let cachedTextDecoder = new TextDecoder('utf-8');
+
+function getStringFromWasm(ptr, len) {
+    return cachedTextDecoder.decode(getUint8Memory().subarray(ptr, ptr + len));
+}
+
+function dropObject(idx) {
+    if (idx < 36) return;
+    heap[idx] = heap_next;
+    heap_next = idx;
+}
+
+function takeObject(idx) {
+    const ret = getObject(idx);
+    dropObject(idx);
+    return ret;
+}
+/**
+*/
+export const Cell = Object.freeze({ Dead:0,Alive:1, });
+/**
+*/
+export class Universe {
+
+    static __wrap(ptr) {
+        const obj = Object.create(Universe.prototype);
+        obj.ptr = ptr;
+
+        return obj;
+    }
+
+    free() {
+        const ptr = this.ptr;
+        this.ptr = 0;
+
+        wasm.__wbg_universe_free(ptr);
+    }
+    /**
+    * @returns {void}
+    */
+    tick() {
+        return wasm.universe_tick(this.ptr);
+    }
+    /**
+    * @returns {Universe}
+    */
+    static new() {
+        return Universe.__wrap(wasm.universe_new());
+    }
+    /**
+    * @returns {number}
+    */
+    width() {
+        return wasm.universe_width(this.ptr) >>> 0;
+    }
+    /**
+    * Set the width of the universe.
+    *
+    * Resets all cells to the dead state.
+    * @param {number} width
+    * @returns {void}
+    */
+    set_width(width) {
+        return wasm.universe_set_width(this.ptr, width);
+    }
+    /**
+    * @returns {number}
+    */
+    height() {
+        return wasm.universe_height(this.ptr) >>> 0;
+    }
+    /**
+    * Set the height of the universe.
+    *
+    * Resets all cells to the dead state.
+    * @param {number} height
+    * @returns {void}
+    */
+    set_height(height) {
+        return wasm.universe_set_height(this.ptr, height);
+    }
+    /**
+    * @returns {number}
+    */
+    cells() {
+        return wasm.universe_cells(this.ptr);
+    }
+    /**
+    * @param {number} row
+    * @param {number} column
+    * @returns {void}
+    */
+    toggle_cell(row, column) {
+        return wasm.universe_toggle_cell(this.ptr, row, column);
+    }
+}
+
+export const __wbg_new_59cb74e423758ede = function() {
+    return addHeapObject(new Error());
+};
+
+export const __wbg_stack_558ba5917b466edd = function(ret, arg0) {
+
+    const retptr = passStringToWasm(getObject(arg0).stack);
+    const retlen = WASM_VECTOR_LEN;
+    const mem = getUint32Memory();
+    mem[ret / 4] = retptr;
+    mem[ret / 4 + 1] = retlen;
+
+};
+
+export const __wbg_error_4bb6c2a97407129a = function(arg0, arg1) {
+    let varg0 = getStringFromWasm(arg0, arg1);
+
+    varg0 = varg0.slice();
+    wasm.__wbindgen_free(arg0, arg1 * 1);
+
+    console.error(varg0);
+};
+
+export const __wbindgen_object_drop_ref = function(arg0) {
+    takeObject(arg0);
+};
+
+export const __wbindgen_throw = function(arg0, arg1) {
+    let varg0 = getStringFromWasm(arg0, arg1);
+    throw new Error(varg0);
+};
