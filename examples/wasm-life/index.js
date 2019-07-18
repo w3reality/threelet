9cf317d367d4a00a0a4e78e8ee7a99aa698a3c04
jayd <j@w3reality.com>
2019-07-18 12:55:58 +0700

    Reorganize functions. Add frame indicator.

--- examples/wasm-life/index.js
+++ examples/wasm-life/index.js
@@ -40,107 +40,13 @@ const universe = Universe.new();
 const width = universe.width();
 const height = universe.height();
 
+//======== begin canvas stuff ========
 // Give the canvas room for all of our cells and a 1px border
 // around each of them.
 const canvas = document.getElementById("game-of-life-canvas");
 canvas.height = (CELL_SIZE + 1) * height + 1;
 canvas.width = (CELL_SIZE + 1) * width + 1;
 const ctx = canvas.getContext('2d');
-
-const fps = new class {
-    constructor() {
-        this.fps = document.getElementById("fps");
-        this.frames = [];
-        this.lastFrameTimeStamp = performance.now();
-    }
-
-    render() {
-        // Convert the delta time since the last frame render into a measure
-        // of frames per second.
-        const now = performance.now();
-        const delta = now - this.lastFrameTimeStamp;
-        this.lastFrameTimeStamp = now;
-        const fps = 1 / delta * 1000;
-
-        // Save only the latest 100 timings.
-        this.frames.push(fps);
-        if (this.frames.length > 100) {
-            this.frames.shift();
-        }
-
-        // Find the max, min, and mean of our 100 latest timings.
-        let min = Infinity;
-        let max = -Infinity;
-        let sum = 0;
-        for (let i = 0; i < this.frames.length; i++) {
-            sum += this.frames[i];
-            min = Math.min(this.frames[i], min);
-            max = Math.max(this.frames[i], max);
-        }
-        let mean = sum / this.frames.length;
-
-        // Render the statistics.
-        this.fps.textContent = `
-Frames per Second:
-         latest = ${Math.round(fps)}
-avg of last 100 = ${Math.round(mean)}
-min of last 100 = ${Math.round(min)}
-max of last 100 = ${Math.round(max)}
-`.trim();
-    }
-};
-
-let animationId = null;
-
-const isPaused = () => {
-    return animationId === null;
-};
-
-const playPauseButton = document.getElementById("play-pause");
-
-const pause = () => {
-    playPauseButton.textContent = "▶";
-    cancelAnimationFrame(animationId);
-    animationId = null;
-};
-
-playPauseButton.addEventListener("click", event => {
-    if (isPaused()) {
-        universe.tick();
-        play();
-    } else {
-        pause();
-    }
-});
-
-let count = 0;
-const renderLoop = () => {
-    fps.render();
-
-    drawGrid();
-    drawCells();
-
-    //========
-    // for (let i = 0; i < 9; i++) { universe.tick(); }
-    //========
-    if (count === 0) {
-        console.log('pausing at count:', count);
-        pause();
-        return;
-    } else {
-        universe.tick();
-    }
-    count++;
-    //========
-
-    animationId = requestAnimationFrame(renderLoop);
-};
-
-const play = () => {
-    playPauseButton.textContent = "⏸";
-    renderLoop();
-};
-
 
 const drawGrid = () => {
     ctx.beginPath();
@@ -155,7 +61,6 @@ const drawGrid = () => {
     }
     ctx.stroke();
 };
-
 
 const _fillCell = (ctx, row, col) => {
     ctx.fillRect(
@@ -199,9 +104,109 @@ canvas.addEventListener("click", event => {
     const canvasTop = (event.clientY - boundingRect.top) * scaleY;
     const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
     const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+
     universe.toggle_cell(row, col);
-    drawCells();
+
     drawGrid();
+    drawCells();
+});
+//======== end canvas stuff ========
+
+
+const fps = new class {
+    constructor() {
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimeStamp = performance.now();
+    }
+
+    render(frame) {
+        // Convert the delta time since the last frame render into a measure
+        // of frames per second.
+        const now = performance.now();
+        const delta = now - this.lastFrameTimeStamp;
+        this.lastFrameTimeStamp = now;
+        const fps = 1 / delta * 1000;
+
+        // Save only the latest 100 timings.
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+        // Find the max, min, and mean of our 100 latest timings.
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+        let mean = sum / this.frames.length;
+
+        // Render the statistics.
+        this.fps.textContent = `
+frame: ${frame}
+fps:
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+    }
+};
+
+let animationId = null;
+
+let _count = 0;
+let _frame = 0;
+const renderLoop = () => {
+    fps.render(_frame);
+
+    console.log('renderLoop(): draw{Grid,Cells}() for frame:', _frame);
+    drawGrid();
+    drawCells();
+
+    //========
+    // for (let i = 0; i < 9; i++) {
+    //     universe.tick();
+    //     _frame++;
+    // }
+    //========
+    if (_count === 0) {
+    // if (_count === 33) {
+        console.log('pausing at _count:', _count);
+        pause();
+        return;
+    } else {
+        universe.tick();
+        _frame++;
+    }
+    _count++;
+    //========
+
+    animationId = requestAnimationFrame(renderLoop);
+};
+
+const playPauseButton = document.getElementById("play-pause");
+const play = () => {
+    playPauseButton.textContent = "⏸";
+    renderLoop();
+};
+const pause = () => {
+    playPauseButton.textContent = "▶";
+    cancelAnimationFrame(animationId);
+    animationId = null;
+};
+playPauseButton.addEventListener("click", event => {
+    if (animationId === null) { // if paused
+        universe.tick();
+        _frame++;
+        play();
+    } else {
+        pause();
+    }
 });
 
 play();
