035aa40c23ad58a4d693e0705d87914c4dc460ee
jayd <j@w3reality.com>
2019-07-31 10:52:00 +0700

    Use plain make command not $(MAKE)

--- examples/rust-canvas-hello/canvas.mk
+++ examples/rust-canvas-hello/canvas.mk
@@ -2,7 +2,7 @@ all: wasm
 
 export CRATE_DIR=./canvas
 export CRATE_NAME=canvas
-MAKE_TARGET=$(MAKE) -f ../wasm/bindgen.mk $@
+MAKE_TARGET=make -f ../wasm/bindgen.mk $@
 wasm:
 	$(MAKE_TARGET) 
 
--- examples/rust-canvas-juliaset/juliaset.mk
+++ examples/rust-canvas-juliaset/juliaset.mk
@@ -2,7 +2,7 @@ all: wasm
 
 export CRATE_DIR=./julia_set
 export CRATE_NAME=julia_set
-MAKE_TARGET=$(MAKE) -f ../wasm/bindgen.mk $@
+MAKE_TARGET=make -f ../wasm/bindgen.mk $@
 wasm:
 	$(MAKE_TARGET) 
 
