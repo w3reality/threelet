1487d10725b665d3279cf211b385d90534ff1d79
jayd <j@w3reality.com>
2019-08-02 10:03:47 +0700

    Refactor and clean up

--- examples/rust-fern-bench/fern/src/lib.rs
+++ examples/rust-fern-bench/fern/src/lib.rs
@@ -37,6 +37,21 @@ impl VertsBuffer {
             colors,
         }
     }
+    pub fn compute_one_triangle(&mut self) {
+        let positions = &mut self.positions;
+        let colors = &mut self.colors;
+
+        // 1 vertex: 4b*3 + 1b*4 = 16 byte
+        let pp = [0.0,0.0,0.0,  0.5,0.0,0.0,  0.0,0.5,0.0];
+        let cc = [255,0,0,255,  0,255,0,255,  0,0,255,255];
+
+        // positions = pp.to_vec(); // debug
+        // colors = cc.to_vec(); // debug
+        //----
+        pp.iter().for_each(|&q| positions.push(q));
+        cc.iter().for_each(|&q| colors.push(q));
+        console::log_2(&"positions:".into(), &format!("{:?}", positions).into());
+    }
     pub fn compute_fern(&mut self) {
         let positions = &mut self.positions;
         let colors = &mut self.colors;
@@ -44,53 +59,41 @@ impl VertsBuffer {
         let max_iter = num_verts / 3;
         console::log_2(&"num_verts:".into(), &format!("{:?}", num_verts).into());
 
-        // 1 vertex: 4b*3 + 1b*4 = 16 byte
-        if num_verts == 3 { // testing a single triangle
-            let pp = [0.0,0.0,0.0,  0.5,0.0,0.0,  0.0,0.5,0.0];
-            let cc = [255,0,0,255,  0,255,0,255,  0,0,255,255];
+        let tri_width = 0.02;
+        let z_variation = 0.01;
+        let base_color = [255, 0, 0, 100];
 
-            // positions = pp.to_vec(); // debug
-            // colors = cc.to_vec(); // debug
-            //----
-            pp.iter().for_each(|&q| positions.push(q));
-            cc.iter().for_each(|&q| colors.push(q));
-            console::log_2(&"positions:".into(), &format!("{:?}", positions).into());
-        } else {
-            let mut x = 0.0;
-            let mut y = 0.0;
-            // let tri_width = 0.5;
-            let tri_width = 0.02;
-            (0..max_iter).for_each(|_i| {
-                let z = 0.0;// !!!!!! thickness * rand
-                let x_shift = 5.0;
+        let f1 = [0.0, 0.0, 0.0, 0.16, 0.0, 0.0, 0.01]; // Stem
+        let f2 = [0.85, 0.04, -0.04, 0.85, 0.0, 1.60, 0.85]; // Successively smaller leaflets
+        let f3 = [0.20, -0.26, 0.23, 0.22, 0.0, 1.60, 0.07]; // Largest left-hand leaflet
+        let f4 = [-0.15, 0.28, 0.26, 0.24, 0.0, 0.44, 0.07]; // Largest right-hand leaflet
+        let prob_acc_1 = f1[6];
+        let prob_acc_2 = f1[6] + f2[6];
+        let prob_acc_3 = f1[6] + f2[6] + f3[6];
 
-                positions.extend_from_slice(
-//                    &[x,y,z,  x+tri_width,y,z,  x,y+tri_width,z]);
-                    &[x+x_shift,y,z,  x+x_shift+tri_width,y,z,  x+x_shift,y+tri_width,z]);
-                colors.extend_from_slice(
-                    &[255,255,0,100,  255,255,0,100,  255,0,0,100]);
+        let mut x = 0.0;
+        let mut y = 0.0;
+        (0..max_iter).for_each(|i| {
+            let z = z_variation * (i as f32) / (max_iter as f32);
 
-                let f1 = [0.0, 0.0, 0.0, 0.16, 0.0, 0.0, 0.01]; // Stem
-                let f2 = [0.85, 0.04, -0.04, 0.85, 0.0, 1.60, 0.85]; // Successively smaller leaflets
-                let f3 = [0.20, -0.26, 0.23, 0.22, 0.0, 1.60, 0.07]; // Largest left-hand leaflet
-                let f4 = [-0.15, 0.28, 0.26, 0.24, 0.0, 0.44, 0.07]; // Largest right-hand leaflet
+            positions.extend_from_slice(
+                &[x,y,z,  x+tri_width,y,z,  x,y+tri_width,z]);
+            colors.extend_from_slice(&base_color);
+            colors.extend_from_slice(&base_color);
+            colors.extend_from_slice(&base_color);
 
-                let p_acc_1 = f1[6];
-                let p_acc_2 = f1[6] + f2[6];
-                let p_acc_3 = f1[6] + f2[6] + f3[6];
-                let p = (rand::thread_rng().gen_range(0, 100) as f32) / 100.0;
-                let cs = match p {
-                    p if p < p_acc_1 => f1,
-                    p if p < p_acc_2 => f2,
-                    p if p < p_acc_3 => f3,
-                    _ => f4,
-                };
-                let _x = cs[0] * x + cs[1] * y + cs[4];
-                let _y = cs[2] * x + cs[3] * y + cs[5];
-                x = _x;
-                y = _y;
-            });
-        }
+            let prob = (rand::thread_rng().gen_range(0, 100) as f32) / 100.0;
+            let cs = match prob {
+                prob if prob < prob_acc_1 => f1,
+                prob if prob < prob_acc_2 => f2,
+                prob if prob < prob_acc_3 => f3,
+                _ => f4,
+            };
+            let _x = cs[0] * x + cs[1] * y + cs[4];
+            let _y = cs[2] * x + cs[3] * y + cs[5];
+            x = _x;
+            y = _y;
+        });
     }
     pub fn get_positions(&self) -> wasm_bindgen::JsValue {
         unsafe {
@@ -102,7 +105,7 @@ impl VertsBuffer {
             JsValue::from(js_sys::Uint8Array::view(&self.colors))
         }
     }
-    pub fn hash(&self) {
+    pub fn test_hash(&self) {
         let sum_positions = self.positions.iter().map(|&q| q as f64).sum::<f64>();
         let sum_colors = self.colors.iter().map(|&q| q as u32).sum::<u32>();
         console::log_2(&"hash:".into(), &format!("{} {}", sum_positions, sum_colors).into());
