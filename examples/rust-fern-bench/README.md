98b1635a2afc2793c43d8d6e3a233b67fa066579
jayd <j@w3reality.com>
2019-11-12 15:55:33 +0700

    Add `wasm-compat` makefile target support for NodeJS and Web Workers

--- examples/rust-fern-bench/README.md
+++ examples/rust-fern-bench/README.md
@@ -1,8 +1,9 @@
 ### Building wasm files
 
 First, make sure the <a href="https://rustwasm.github.io/wasm-pack/installer/" target="_blank" rel="noopener">wasm-pack</a> command is
-installed in the system.  Then, build `<name>_bg.wasm` and `<name>.export.js` (where `<name>` is the Rust crate's name) as follows:
+installed in the system.  Then, build `<name>_bg.wasm` and `<name>.exports.js` (where `<name>` is the Rust crate's name) as follows:
 
 ```sh
+$ cd ../.. && npm install && cd -  # for the `wasm-compat` target in Makefile
 $ make
 ```
--- examples/wasm/bindgen.mk
+++ examples/wasm/bindgen.mk
@@ -1,7 +1,13 @@
 wasm:
 	cd $(CRATE_DIR) && wasm-pack build
 	cp $(CRATE_DIR)/pkg/$(CRATE_NAME)_bg.wasm .
-	cat $(CRATE_DIR)/pkg/$(CRATE_NAME).js | sed -e "s/import \* as wasm.*/export const wasm = {_timestamp: performance.now()}; \/\/ to be populated by loadWasmBindgen()/g" > $(CRATE_NAME).export.js
+	cat $(CRATE_DIR)/pkg/$(CRATE_NAME).js | sed -e "s/import \* as wasm.*/export const wasm = {_timestamp: performance.now()}; \/\/ to be populated by loadWasmBindgen()/g" > $(CRATE_NAME).exports.js
+
+# this variant generates `*.exports.js` that is compatible with NodeJS and Web Workers.
+wasm-compat:
+	cd $(CRATE_DIR) && wasm-pack build
+	cp $(CRATE_DIR)/pkg/$(CRATE_NAME)_bg.wasm .
+	$(WASM_TOOLS_DIR)/gen-bindgen-exports $(CRATE_DIR) $(CRATE_NAME) $(OUT_DIR)
 
 clean:
-	rm -f $(CRATE_NAME)_bg.wasm $(CRATE_NAME).export.js
+	rm -f $(CRATE_NAME)_bg.wasm $(CRATE_NAME).exports.js
