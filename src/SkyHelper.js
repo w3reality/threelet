31e0e50b024a8f9c9271e44fde8ce8ea17e132ac
j-devel <j@w3reality.com>
2020-11-22 10:44:54 +0700

    Update 'examples/simple-static'

--- src/SkyHelper.js
+++ src/SkyHelper.js
@@ -6,14 +6,13 @@ class SkyHelper {
     constructor(classSky) {
         this._classSky = classSky;
         this._effectController = {
-            turbidity: 10,
-            rayleigh: 2,
-            mieCoefficient: 0.005,
-            mieDirectionalG: 0.8,
-            luminance: 1,
-            inclination: 0.49, // elevation / inclination
-            azimuth: 0.25, // Facing front,
-            sun: ! true
+            turbidity: 5.2,
+            rayleigh: 0.01,
+            mieCoefficient: 0.002,
+            mieDirectionalG: 0.9,
+            inclination: 0.02,
+            azimuth: 0.1,
+            sun: !true
         };
         this._updateUniforms = null;
     }
@@ -42,7 +41,6 @@ class SkyHelper {
             var uniforms = sky.material.uniforms;
             uniforms[ "turbidity" ].value = effectController.turbidity;
             uniforms[ "rayleigh" ].value = effectController.rayleigh;
-            uniforms[ "luminance" ].value = effectController.luminance;
             uniforms[ "mieCoefficient" ].value = effectController.mieCoefficient;
             uniforms[ "mieDirectionalG" ].value = effectController.mieDirectionalG;
             var theta = Math.PI * ( effectController.inclination - 0.5 );
@@ -60,7 +58,6 @@ class SkyHelper {
         // gui.add( effectController, "rayleigh", 0.0, 4, 0.001 ).onChange( guiChanged );
         // gui.add( effectController, "mieCoefficient", 0.0, 0.1, 0.001 ).onChange( guiChanged );
         // gui.add( effectController, "mieDirectionalG", 0.0, 1, 0.001 ).onChange( guiChanged );
-        // gui.add( effectController, "luminance", 0.0, 2 ).onChange( guiChanged );
         // gui.add( effectController, "inclination", 0, 1, 0.0001 ).onChange( guiChanged );
         // gui.add( effectController, "azimuth", 0, 1, 0.0001 ).onChange( guiChanged );
         // gui.add( effectController, "sun" ).onChange( guiChanged );
