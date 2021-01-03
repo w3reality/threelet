c563fbd666a145f08b0ce60c3e149270c7c27957
j-devel <j@w3reality.com>
2021-01-03 16:16:52 +0700

    Deprecate bindgen.mk stuff

--- examples/wasm/bindgen.mk
+++ examples/wasm/bindgen.mk
@@ -1,10 +1,12 @@
 CRATE_NAME:=$(subst -,_,$(CRATE_NAME))
 
+# DEPRECATED: Use deps/load-wbg.js instead
 wasm:
 	cd $(CRATE_DIR) && wasm-pack build
 	cp $(CRATE_DIR)/pkg/$(CRATE_NAME)_bg.wasm .
 	cat $(CRATE_DIR)/pkg/$(CRATE_NAME).js | sed -e "s/import \* as wasm.*/export const wasm = {_timestamp: performance.now()}; \/\/ to be populated by loadWasmBindgen()/g" > $(CRATE_NAME).exports.js
 
+# DEPRECATED: Use the wasm-mt crate instead
 # this variant generates `*.exports.js` that is compatible with NodeJS and Web Workers.
 wasm-compat:
 	cd $(CRATE_DIR) && wasm-pack build $(WASM_PACK_BUILD_FLAGS)
