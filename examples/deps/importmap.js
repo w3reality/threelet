cb35242cb924f4185635bd448bc4e779068cbd8c
j-devel <j@w3reality.com>
2022-02-07 09:09:35 +0700

    Add 'deps/importmap.js'

---
+++ examples/deps/importmap.js
@@ -0,0 +1,11 @@
+{
+  "imports": {
+    "stats": "../deps/three/examples/jsm/libs/stats.module.js",
+    "orbit-controls": "../deps/three/examples/jsm/controls/OrbitControls.js",
+    "sky": "../deps/three/examples/jsm/objects/Sky.js",
+    "gltf-loader": "../deps/three/examples/jsm/loaders/GLTFLoader.js",
+    "collada-loader": "../deps/three/examples/jsm/loaders/ColladaLoader.js",
+    "fbx-loader": "../deps/three/examples/jsm/loaders/FBXLoader.js",
+    "three": "../deps/three/build/three.module.js"
+  }
+}
